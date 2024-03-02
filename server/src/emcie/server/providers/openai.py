663739cd0b76ca735015768b68c7839fbd6569c6
Yam Marcovitz <yam@emcie.co>
2024-03-02 23:28:12 +0200

    Add license

--- server/src/emcie/server/providers/openai.py
+++ server/src/emcie/server/providers/openai.py
@@ -1,3 +1,12 @@
+# Copyright (c) 2024 Emcie
+# All rights reserved.
+#
+# This file and its contents are the property of Emcie and are strictly confidential.
+# No part of this file may be reproduced, distributed, or transmitted in any form or by any means,
+# including photocopying, recording, or other electronic or mechanical methods,
+# without the prior written permission of Emcie.
+#
+# Website: https://emcie.co
 from typing import Any, AsyncIterator, Iterable, List
 from openai import AsyncOpenAI, AsyncAzureOpenAI
 from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,3 +1,12 @@
+# Copyright (c) 2024 Emcie
+# All rights reserved.
+#
+# This file and its contents are the property of Emcie and are strictly confidential.
+# No part of this file may be reproduced, distributed, or transmitted in any form or by any means,
+# including photocopying, recording, or other electronic or mechanical methods,
+# without the prior written permission of Emcie.
+#
+# Website: https://emcie.co
 from typing import Any, AsyncIterator, Dict
 from fastapi import status
 from fastapi.testclient import TestClient

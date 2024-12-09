2c466a23036ca6bd7ee0870b059f2d64847d7111
Menachem Brichta <menachem@emcie.co>
2024-12-09 10:51:56 +0000

    select a customer

--- src/parlant/api/chat/src/components/agents-list/agent-list.tsx
+++ src/parlant/api/chat/src/components/agents-list/agent-list.tsx
@@ -1,9 +1,10 @@
 import { SessionInterface } from '@/utils/interfaces';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { useSession } from '../chatbot/chatbot';
 
 import AgentAvatar from '../agent-avatar/agent-avatar';
 import { spaceClick } from '@/utils/methods';
+import { DialogDescription, DialogHeader, DialogTitle } from '../ui/dialog';
 
 export const NEW_SESSION_ID = 'NEW_SESSION';
 
@@ -15,27 +16,43 @@ const newSessionObj: SessionInterface = {
     id: NEW_SESSION_ID
 };
 
+
 const AgentList = (): ReactNode => {
-    const {setAgentId, closeDialog, agents, setSessionId, setNewSession} = useSession();
+    const {setAgentId, closeDialog, agents, setSessionId, setNewSession, customers} = useSession();
+    const [agent, setAgent] = useState('');
 
     const selectAgent = (agentId: string): void => {
-        setAgentId(agentId);
-        setNewSession(newSessionObj);
+        setAgent(agentId);
+    };
+
+    const selectCustomer = (customerId: string) => {
+        setAgentId(agent);
+        setNewSession({...newSessionObj, agent_id: agent, customer_id: customerId});
         setSessionId(newSessionObj.id);
         closeDialog();
     };
 
     return (
-        <div className='flex flex-col overflow-auto'>
-            {agents?.map(agent => (
-                <div data-testid="agent" tabIndex={0} onKeyDown={spaceClick} role='button' onClick={() => selectAgent(agent.id)} key={agent.id} className='cursor-pointer hover:bg-[#FBFBFB] min-h-[78px] h-[78px] w-full border-b-[0.6px] border-b-solid border-b-[#EBECF0] flex items-center ps-[30px] pe-[20px]'>
-                    <AgentAvatar agent={agent} tooltip={false}/>
-                    <div>
-                        <div className='text-[16px] font-medium'>{agent.name}</div>
-                        <div className='text-[14px] font-light text-[#A9A9A9]'>(id={agent.id})</div>
+        <div>
+            <DialogHeader>
+                <DialogTitle>
+                    <div className='h-[68px] w-full flex justify-between items-center ps-[30px] pe-[20px] border-b-[#EBECF0] border-b-[0.6px]'>
+                        <DialogDescription className='text-[16px] font-bold'>{agent ? 'Select a Customer' : 'Select an Agent'}</DialogDescription>
+                        <img role='button' tabIndex={0} onKeyDown={spaceClick} onClick={closeDialog} className='cursor-pointer rounded-full hover:bg-[#F5F6F8] p-[10px]' src="icons/close.svg" alt="close" height={30} width={30}/>
                     </div>
-                </div>
-            ))}
+                </DialogTitle>
+            </DialogHeader>
+            <div className='flex flex-col overflow-auto'>
+                {(agent ? customers : agents)?.map(entity => (
+                    <div data-testid="agent" tabIndex={0} onKeyDown={spaceClick} role='button' onClick={() => agent ? selectCustomer(entity.id) : selectAgent(entity.id)} key={entity.id} className='cursor-pointer hover:bg-[#FBFBFB] min-h-[78px] h-[78px] w-full border-b-[0.6px] border-b-solid border-b-[#EBECF0] flex items-center ps-[30px] pe-[20px]'>
+                        <AgentAvatar agent={entity} tooltip={false}/>
+                        <div>
+                            <div className='text-[16px] font-medium'>{entity.name}</div>
+                            <div className='text-[14px] font-light text-[#A9A9A9]'>(id={entity.id})</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };

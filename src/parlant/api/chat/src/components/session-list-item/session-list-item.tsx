d1d72d56fadba44e9023ba0fe145fbbead3112e5
Menachem Brichta <menachem@emcie.co>
2025-02-18 18:28:39 +0000

    start refactoring
    Signed-off-by: Menachem Brichta <menachem@emcie.co>

--- src/parlant/api/chat/src/components/session-list-item/session-list-item.tsx
+++ src/parlant/api/chat/src/components/session-list-item/session-list-item.tsx
@@ -7,7 +7,7 @@ import {toast} from 'sonner';
 import {SessionInterface} from '@/utils/interfaces';
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from '../ui/dropdown-menu';
 import {getDateStr, getTimeStr} from '@/utils/date';
-import styles from './session.module.scss';
+import styles from './session-list-item.module.scss';
 import AgentAvatar from '../agent-avatar/agent-avatar';
 import {NEW_SESSION_ID} from '../chat-header/chat-header';
 import {spaceClick} from '@/utils/methods';
@@ -28,7 +28,7 @@ interface Props {
 
 export const DeleteDialog = ({session, closeDialog, deleteClicked}: {session: SessionInterface; closeDialog: () => void; deleteClicked: (e: React.MouseEvent) => Promise<void> | undefined}) => (
 	<div data-testid='deleteDialogContent'>
-		<Session session={session} disabled />
+		<SessionListItem session={session} disabled />
 		<div className='h-[80px] flex items-center justify-end pe-[18px]'>
 			<Button data-testid='cancel-delete' onClick={closeDialog} className='h-[46px] w-[96px] !bg-white text-[#656565] hover:text-[#151515] rounded-[6px] py-[12px] px-[24px] me-[10px] text-[16px] font-normal border'>
 				Cancel
@@ -40,7 +40,7 @@ export const DeleteDialog = ({session, closeDialog, deleteClicked}: {session: Se
 	</div>
 );
 
-export default function Session({session, isSelected, refetch, editingTitle, setEditingTitle, tabIndex, disabled}: Props): ReactElement {
+export default function SessionListItem({session, isSelected, refetch, editingTitle, setEditingTitle, tabIndex, disabled}: Props): ReactElement {
 	const sessionNameRef = useRef<HTMLInputElement>(null);
 	const [agents] = useAtom(agentsAtom);
 	const [customers] = useAtom(customersAtom);
---
+++ src/parlant/api/chat/src/components/session-view/session-view-header/session-view-header.tsx
@@ -0,0 +1,40 @@
+import AgentAvatar from '@/components/agent-avatar/agent-avatar';
+import HeaderWrapper from '@/components/header-wrapper/header-wrapper';
+import CopyText from '@/components/ui/custom/copy-text';
+import {agentAtom, customerAtom, sessionAtom} from '@/store';
+import {AgentInterface} from '@/utils/interfaces';
+import {useAtom} from 'jotai';
+import {twJoin} from 'tailwind-merge';
+
+const SessoinViewHeader = () => {
+	const [session] = useAtom(sessionAtom);
+	const [agent] = useAtom(agentAtom);
+	const [customer] = useAtom(customerAtom);
+	return (
+		<HeaderWrapper className={twJoin('border-e')}>
+			{session?.id && (
+				<div className='w-full flex items-center h-full'>
+					<div className='h-full flex-1 flex items-center ps-[24px] border-e'>
+						<AgentAvatar agent={agent as AgentInterface} />
+						<div>
+							<div>{agent?.name}</div>
+							<div className='group flex items-center gap-[3px] text-[14px] font-normal'>
+								<CopyText preText='Agent ID:' text={` ${agent?.id}`} textToCopy={agent?.id} />
+							</div>
+						</div>
+					</div>
+					<div className='h-full flex-1 flex items-center ps-[24px]'>
+						<AgentAvatar agent={customer as AgentInterface} />
+						<div>
+							<div>{(customer?.id == 'guest' && 'Guest') || customer?.name}</div>
+							<div className='group flex items-center gap-[3px] text-[14px] font-normal'>
+								<CopyText preText='Customer ID:' text={` ${customer?.id}`} textToCopy={customer?.id} />
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</HeaderWrapper>
+	);
+};
+export default SessoinViewHeader;

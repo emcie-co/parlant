f32314c75e41759b788ec92bfb537825d16fd361
Dor Zohar <dor@emcie.co>
2025-08-30 16:37:40 +0000

    Fix mypy issue
    Signed-off-by: Dor Zohar <dor@emcie.co>

--- src/parlant/core/background_tasks.py
+++ src/parlant/core/background_tasks.py
@@ -152,7 +152,7 @@ class BackgroundTaskService:
     def _cancel_if_not_suppressed(self, task: Task, message: str) -> None:
         task_tag = getattr(task, "__parlant_background_task_tag__", None)
 
-        if cancellations.is_contextual_suppression_latch_enabled(task_tag):
+        if task_tag and cancellations.is_contextual_suppression_latch_enabled(task_tag):
             self._logger.info(f"{type(self).__name__}: Task cancellation was suppressed '{task}'")
             return
 

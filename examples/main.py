d23a53a957a27c240097d3838a9bd2e9239f92b0
Satyam Mishra <satyammishra9050@gmail.com>
2025-10-10 12:52:26 +0530

    update examples/main.py file to demo select multiple gemini models
    Signed-off-by: Satyam Mishra <satyammishra9050@gmail.com>

--- examples/main.py
+++ examples/main.py
@@ -1,15 +1,10 @@
-import sys
-from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
-
-
 import asyncio
-from src.parlant import sdk as p
+import parlant.sdk as p
 
 
 async def main():
     async with p.Server(
+        # Add multiple fallback models, in order of preference.
         nlp_service=p.NLPServices.gemini(
             model_name=["gemini-2.0-flash-lite", "gemini-2.5-flash"]
         )

1b08055c4853927149b1b3c39a52d21d9f288bc9
Bar Karov <bar@emcie.co>
2025-10-20 15:12:34 +0000

    small change in import
    Signed-off-by: Bar Karov <bar@emcie.co>

--- tests/core/unstable/engines/alpha/test_previously_applied_actionable_batch.py
+++ tests/core/unstable/engines/alpha/test_previously_applied_actionable_batch.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from collections.abc import Container
+from lagom import Container
 from datetime import datetime, timezone
 
 from pytest import fixture

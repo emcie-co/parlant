edc80224a704d3a2727c07fb5e35a016c164a2ac
yrk <2493404415@qq.com>
2025-10-22 10:19:38 +0800

    add information
    Signed-off-by: yrk <2493404415@qq.com>

--- src/parlant/adapters/nlp/modelscope_service.py
+++ src/parlant/adapters/nlp/modelscope_service.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Maintainer: Rongkun Yan <2493404415@qq.com>
 
 from __future__ import annotations
 import time

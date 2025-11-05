b55792281464909365a6a7d261be2be11d6dc59c
Taooo-habitus <ttan@habitus.dk>
2025-11-05 14:38:53 +0100

    :memo: Docs: Add the maintainer's info
    Signed-off-by: Taooo-habitus <ttan@habitus.dk>

--- src/parlant/adapters/db/snowflake_db.py
+++ src/parlant/adapters/db/snowflake_db.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Maintainer: Tao Tang <ttan@habitus.dk>
+
 from __future__ import annotations
 
 import asyncio

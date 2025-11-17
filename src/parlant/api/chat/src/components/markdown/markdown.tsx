7268a5e2208e73fe25e2acee7bf468af50090528
Menachem Brichta <menachem@emcie.co>
2025-11-17 14:24:24 +0000

    Add rehype-raw plugin for enhanced Markdown processing
    Signed-off-by: Menachem Brichta <menachem@emcie.co>

--- src/parlant/api/chat/src/components/markdown/markdown.tsx
+++ src/parlant/api/chat/src/components/markdown/markdown.tsx
@@ -1,17 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import rehypeHighlight from 'rehype-highlight';
-import 'highlight.js/styles/github.css';
 import remarkBreaks from 'remark-breaks';
+import rehypeHighlight from 'rehype-highlight';
+import rehypeRaw from 'rehype-raw';
+import 'highlight.js/styles/github.css';
 import styles from '../message/message.module.scss';
 import {twMerge} from 'tailwind-merge';
 
 const Markdown = ({children, className}: {children: string; className?: string}) => {
 	return (
 		<ReactMarkdown
-			components={{p: 'div', img: ({node, ...props}) => <img {...props} loading='lazy' alt='' />}}
-			rehypePlugins={[rehypeHighlight]}
+			components={{
+				p: 'div',
+				img: ({node, ...props}) => <img {...props} loading='lazy' alt='' />
+			}}
+			rehypePlugins={[rehypeHighlight, rehypeRaw]}
 			remarkPlugins={[remarkGfm, remarkBreaks]}
 			className={twMerge('leading-[19px]', styles.markdown, className)}>
 			{children}

ddceb40799db7a8aefe6b54c7496e501199575c9
Dor Zohar <dor@emcie.co>
2026-02-08 21:48:31 +0200

    Fix tests
    Signed-off-by: Dor Zohar <dor@emcie.co>

--- src/parlant/sdk.py
+++ src/parlant/sdk.py
@@ -4673,6 +4673,7 @@ class Server:
                                     tool_name=retriever_id,
                                 ).to_string(),
                                 arguments={},
+                                rationale="Retriever result",
                                 result=tool_result,
                             )
                         ]
